c5a44d3c738455889dfc8303706007d4e0d0ec4e
Venkata Mani Naga Rakesh Buddhiraju <nagarakesh4@gmail.com>
2017-04-17 06:02:07 -0700

    add react component and adhoc way of routing to hash URL's

---
+++ src/components/homePage.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var React = require('react');
+
+var Homepage = React.createClass({
+	render: function(){
+		return (
+			<div className="jumbotron">
+				<h3>Centerra Apartments - Apt1406</h3>
+				<p>Owner details: Venkata Buddhiraju, San Jose, CA</p>
+			</div>
+		);
+	}
+});
+
+module.exports = Homepage;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,31 @@
 $ = jquery = require('jquery'); //set jquery to both 'jquery' and '$'
 //bootstrap expects jquery to be in the global namespace
 
-var app = console.log("this is a test file in console log");
+var React = require('react');
+var HomePage = require('./components/homePage');
+var AboutPage = require('./components/about/aboutPage');
 
-module.exports = app;
+var App = React.createClass({
+	render: function(){
+		var Child;
+		
+		switch(this.props.route){
+			case 'about': Child = AboutPage; break;
+			default: Child = HomePage;
+	    }
+		
+		return(
+			<div>
+				<Child />
+			</div>
+		);
+	}
+});
+
+function render(){
+	var route = window.location.hash.substr(1);
+	React.render(<App route={route} />, document.getElementById("app"));
+}
+
+window.addEventListener('hashchange', render);
+render();
